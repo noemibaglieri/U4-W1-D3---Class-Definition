ad59072816319173a1cddd019fcf550484970b14
Noemi Baglieri <noemibaglieri@gmail.com>
2025-06-19 13:54:26 +0200

    2nd task done

--- src/task2/Sim.java
+++ src/task2/Sim.java
@@ -6,17 +6,24 @@ public class Sim {
 
     private String phoneNumber;
     private int credit;
-    private String[] callList;
+    private Call[] callList;
 
     public Sim(String phoneNumber) {
         this.phoneNumber = phoneNumber;
         this.credit = 0;
-        this.callList = new String[5];
+        this.callList = new Call[5];
+    }
+
+    public void setCallList(Call[] callList) {
+        this.callList = callList;
     }
 
     public void printSim() {
         System.out.println("This Sim phone number is: " + phoneNumber);
         System.out.println("It has: " + credit + " credit");
-        System.out.println("The last 5 calls made were: " + Arrays.toString(callList));
+        System.out.println("The last 5 calls made were: " );
+        for(Call call : callList) {
+            System.out.println(call.getCalledNumber());
+        }
     }
 }
--- src/task2/Task2.java
+++ src/task2/Task2.java
@@ -1,8 +1,27 @@
 package task2;
 
+import java.util.Arrays;
+
 public class Task2 {
     public static void main(String[] args) {
         Sim voxi = new Sim("3501151953");
+
+        Call callRobin = new Call("38850116741", 10);
+        Call callTed = new Call("34543976513", 2);
+        Call callBarney = new Call("37183945134", 54);
+        Call callLily = new Call("35011234311", 43);
+        Call callMarshall = new Call("33894392384", 5);
+
+        Call[] lastFiveCalls = new Call[5];
+        lastFiveCalls[0] = callRobin;
+        lastFiveCalls[1] = callTed;
+        lastFiveCalls[2] = callBarney;
+        lastFiveCalls[3] = callLily;
+        lastFiveCalls[4] = callMarshall;
+
+        voxi.setCallList(lastFiveCalls);
         voxi.printSim();
+
+
     }
 }
